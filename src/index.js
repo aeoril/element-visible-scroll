72b6259f1c12c4cbd90579630f88d45dc6a276cd
Aeoril <aeoril@ic3dimensions.com>
2017-11-12 20:02:46 -0600

    Updated to reworked rAFRateLimit

--- src/index.js
+++ src/index.js
@@ -9,42 +9,42 @@
   var output;
   var scrolls = 0;
 
-  function setInnerText(elem, str) {
-    elem.innerText = str;
-    console.log(str + ' ' + scrolls);
+  function scroll() {
+    var triggerRect = scroll.args.triggerElem.getBoundingClientRect();
+
+    if (triggerRect.bottom > 0 && triggerRect.top < (window.visualViewport ? window.visualViewport.height : window.innerHeight)) {
+      output = 'Visible';
+    } else {
+      output = 'Not visible';
+    }
+    //if (output !== prevOutput) {
+      scroll.args.outputElem.innerText = output;
+      console.log(output + ' ' + scroll.args.scrolls);
+
+      //prevOutput = output;
+    //}
+
     scrolls = 0;
   }
 
+  var rAFRateLimitedScroll = rAFRateLimit(scroll, 0);
+
   window.addEventListener('load', function () {
+
     var triggerElem = document.getElementById('trigger');
     var outputElem = document.getElementById('output');
 
-    var rAFRateLimitedOutput = rAFRateLimit(setInnerText.bind(null, outputElem), 0);
-
-    function scroll() {
-      var triggerRect = triggerElem.getBoundingClientRect();
-
-      if (triggerRect.bottom > 0 && triggerRect.top < (window.visualViewport ? window.visualViewport.height : window.innerHeight)) {
-        output = 'Visible';
-      } else {
-        output = 'Not visible';
-      }
-      //if (output !== prevOutput) {
-        scrolls++;
-        rAFRateLimitedOutput(output);
-        prevOutput = output;
-      //}
-    }
-
-    window.addEventListener('scroll', scroll, false);
-
-    scroll();
+    window.addEventListener('scroll', function () {
+      rAFRateLimitedScroll({ triggerElem: triggerElem, outputElem: outputElem, scrolls: ++scrolls });
+    }, false);
 
     outputElem.addEventListener('click', function () {
 
-      for (var ii = 0; ii < 100; ii++) {
-        scroll();
+      for (var ii = 0; ii < 10000; ii++) {
+        rAFRateLimitedScroll({ triggerElem: triggerElem, outputElem: outputElem, scrolls: ++scrolls });
       }
     }, false);
+    
+    rAFRateLimitedScroll({ triggerElem: triggerElem, outputElem: outputElem, scrolls: ++scrolls });
   }, false);
 }());
